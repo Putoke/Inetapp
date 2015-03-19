40dfca52cc93554a27b2d1d3cc88fb5465ccf7b1
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-03-19 17:12:05 +0100

    Added cookie store management

--- app/src/main/java/com/training/superior/superiortraining/Models/ScheduleTask.java
+++ app/src/main/java/com/training/superior/superiortraining/Models/ScheduleTask.java
@@ -1,11 +1,13 @@
 package com.training.superior.superiortraining.Models;
 
 import android.os.AsyncTask;
+import android.webkit.CookieManager;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.cookie.BasicClientCookie;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,6 +24,19 @@ public class ScheduleTask extends AsyncTask<Void, Void, Boolean> {
 
         boolean success;
         HttpClient httpClient = new DefaultHttpClient();
+
+        //Load saved cookies from cookie store.
+        String[] keyValueSets = CookieManager.getInstance().getCookie("http://u-shell.csc.kth.se:8000").split(";");
+        for(String cookie : keyValueSets)
+        {
+            System.out.println(cookie.toString());
+            String[] keyValue = cookie.split("=");
+            String key = keyValue[0];
+            String value = "";
+            if(keyValue.length>1) value = keyValue[1];
+            ((DefaultHttpClient)httpClient).getCookieStore().addCookie(new BasicClientCookie(key, value));
+        }
+
         try {
             HttpPost request = new HttpPost("http://u-shell.csc.kth.se:8000/login");
 
